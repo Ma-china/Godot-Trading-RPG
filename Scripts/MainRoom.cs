5715597139f82aac0a0a0607abeb2c10b77cf3dc
Ricky Hao <r.hao-1@student.tudelft.nl>
2026-06-10 17:00:29 +0200

    added money feature

--- Scripts/MainRoom.cs
+++ Scripts/MainRoom.cs
@@ -6,6 +6,8 @@ public partial class MainRoom : Node
 	// Called when the node enters the scene tree for the first time.
 	[Export]
 	public float cameraZoom = 1.5f;
+	
+	public int money = 0;
 	public override void _Ready()
 	{
 		if (Multiplayer.IsServer())
@@ -21,5 +23,17 @@ public partial class MainRoom : Node
 		player.Name = $"Player_{Multiplayer.GetUniqueId()}";
 		player.SetMultiplayerAuthority((int)Multiplayer.GetUniqueId());
 		GetTree().CurrentScene.AddChild(player);
+	}
+	
+	//Cheats
+	public override void _Input(InputEvent @event)
+	{
+		if (@event.IsActionPressed("cheat_money"))
+		{
+			money = money + 1;
+			GD.Print("Money: " + money);
+			Label moneyLabel = GetNode<Label>("CanvasLayer/MoneyLabel");
+			moneyLabel.Text = "$" + money;
+		}
 	}
 }
